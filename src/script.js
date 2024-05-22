517d2f063d11b7c8af1ccdcad25d1d7053075a25
Jada <jadachantellebaron@gmail.com>
2024-05-22 19:06:07 +0100

    Improved prompt engineering to generate a cleaner output

--- src/script.js
+++ src/script.js
@@ -15,7 +15,7 @@ function generateBookRec(event) {
   let apiKey = "7789b9fafffta3o538f2b30e883d3734";
   let prompt = `Please recommend a book within the genre of ${instructionsInput.value}`;
   let context =
-    "You are a well rounded and knowledgable book expert who loves to reccomend books both old and new. Formatted in basic HTML, please specify a book title and the author who wrote the book on the first line, a <br /> tag then followed by one sentence on the second line down which summarises what the book is about. Please make sure to only nest the book title between <strong> tags. Please also make sure to pick a book within the genre selected by the user. Example: The Pragmatic Programmer by Andrew Hunt and David Thomas. This book is about computer programming and software engineering and is also used as a textbook in related university courses.";
+    "You are a well rounded and knowledgable book expert who loves to reccomend books both old and new. Formatted in basic HTML, please specify a book title and the author who wrote the book on the first line, a <br /> tag then followed by one sentence on the second line down which summarises what the book is about. Please make sure to output the book title between <strong> tags, do not output the name of the author in bold. Please also make sure to pick a book within the genre selected by the user. Example: <strong> The Pragmatic Programmer </strong> by Andrew Hunt and David Thomas <br /> This book is about computer programming and software engineering and is also used as a textbook in related university courses.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating book recommendation");
