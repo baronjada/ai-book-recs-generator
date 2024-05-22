1c95542ab3319785a0cc7887e0113c992892bc54
Jada <jadachantellebaron@gmail.com>
2024-05-22 22:19:48 +0100

    Improving user experience by adding classList to hide book-rec container until user submits form

--- src/script.js
+++ src/script.js
@@ -18,6 +18,9 @@ function generateBookRec(event) {
     "You are a well rounded and knowledgable book expert who loves to reccomend books both old and new. Formatted in basic HTML, please specify a book title and the author who wrote the book on the first line, a <br /> tag then followed by one sentence on the second line down which summarises what the book is about. Please make sure to output the book title between <strong> tags, do not output the name of the author in bold. Please also make sure to pick a book within the genre selected by the user. Example: <strong> The Pragmatic Programmer </strong> by Andrew Hunt and David Thomas <br /> This book is about computer programming and software engineering and is also used as a textbook in related university courses.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let bookRecElement = document.querySelector("#book-rec");
+  bookRecElement.classList.remove("hidden");
+
   console.log("Generating book recommendation");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
